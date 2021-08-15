664a70e4f016bc953d35b29b8d707c9c67232dfe
marco-colonna <mcolonna107@gmail.com>
2021-08-15 18:22:16 -0400

    Add `intro()` function to memory.py

---
+++ memory.py
@@ -0,0 +1,10 @@
+# print welcome screen and instructions
+def intro():
+    print('\nWelcome to Concentration!\n')
+    print('This is a memory game in which you guess two cards.')
+    print('If the faces of the cards match, you win the pair')
+    print('and they remain face up. Don\'t waste a turn!\n')
+
+    input('Enter any number to CONTINUE: ')
+
+intro()
