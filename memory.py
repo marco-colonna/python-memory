19c4f193485f7a811af6076a40ba0e9fa7df6652
marco-colonna <mcolonna107@gmail.com>
2021-08-15 22:20:06 -0400

    Remove function prototypes from memory.py

--- memory.py
+++ memory.py
@@ -1,25 +1,7 @@
-# print welcome screen and instructions
-def intro():
-    print('\nWelcome to Concentration!\n')
-    print('This is a memory game in which you guess two cards.')
-    print('If the faces of the cards match, you win the pair')
-    print('and they remain face up. Don\'t waste a turn!\n')
+# intro
+print('''\nWelcome to Concentration!\n
+This is a memory game in which you guess two cards.
+If the faces of the cards match, you win the pair
+and they remain face up. Don\'t waste a turn!\n''')
 
-    input('Enter any number to CONTINUE: ')
-
-def print_board():
-    print()
-
-def guess():
-    print()
-
-def check():
-    print()
-
-def won():
-    print()
-
-def main():
-    print()
-
-intro()
+input('Press ENTER to continue: ')
