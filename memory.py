4903ef8c5ddaee0fcd295c968023270fa8438ee8
marco-colonna <mcolonna107@gmail.com>
2021-08-15 22:59:39 -0400

    Add `print_cards()` function to memory.py

--- memory.py
+++ memory.py
@@ -1,29 +1,31 @@
+def print_cards(cards, show):
+    print()
+
+    for index, card in enumerate(cards):
+        if show[index] == False:
+            print(' ■', end='')
+        else:
+            print(' ' + card, end='')
+
+        if (index + 1) % 4 == 0:
+            print()
+
 # intro
 print('''\nWelcome to Concentration!\n
 This is a memory game in which you guess two cards.
 If the faces of the cards match, you win the pair
 and they remain face up. Don\'t waste a turn!\n''')
 
-input('Press ENTER to continue: ')
+input('Enter any number to CONTINUE: ')
 
 # represents the face of each card
 cards = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8']
 # represents whether to display the face of the card
-show = [True]*16
+show = [False]*16
 # represents the number of matches
 matches = 0
 # represents the number of turns
 turns = 0
 
 # print cards
-print()
-for index, card in enumerate(cards):
-    if show[index] == False:
-        print(' ■', end='')
-    else:
-        print(' ' + card, end='')
-    
-    if (index + 1) % 4 == 0:
-        print()
-    
-print()
+print_cards(cards, show)
