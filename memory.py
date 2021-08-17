af29efaea72873d982bcbbc9c128d71d89089337
marco-colonna <mcolonna107@gmail.com>
2021-08-17 17:13:21 -0400

    Standardize and fix string formatting in memory.py

--- memory.py
+++ memory.py
@@ -3,9 +3,9 @@ def print_cards(cards, show):
 
     for index, card in enumerate(cards):
         if show[index] == False:
-            print(' ■', end='')
+            print('■', end=' ')
         else:
-            print(' ' + card, end='')
+            print(card, end=' ')
 
         if (index + 1) % 4 == 0:
             print()
@@ -16,20 +16,20 @@ def get_guess(show):
         guess = input('\nGuess a card: ')
         # numeric
         if guess.isnumeric() == False:
-            print('Invalid input. Please try again.')
+            print('\nInvalid input. Please try again.')
             guess = 0
             continue
         
         guess = int(guess)
         # out of bounds
         if guess < 1 or guess > 16:
-            print('Invalid input. Please try again.')
+            print('\nInvalid input. Please try again.')
             guess = 0
             continue
         
         # already matched/chosen
         if show[guess - 1]:
-            print('Invalid input. Please try again.')
+            print('\nInvalid input. Please try again.')
             guess = 0
             continue
     else:
@@ -40,9 +40,9 @@ def get_guess(show):
 print('''\nWelcome to Concentration!\n
 This is a memory game in which you guess two cards.
 If the faces of the cards match, you win the pair
-and they remain face up. Don\'t waste a turn!\n''')
+and they remain face up. Don\'t waste a turn!''')
 
-input('Enter any number to CONTINUE: ')
+input('\nEnter any number to CONTINUE: ')
 
 # represents the face of each card
 cards = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8']
@@ -92,5 +92,5 @@ while matches != 8:
             print('\nThanks for playing!\n')
             quit()
 else:
-    print('\nCongratulations! Well done!\n')
-    print('Number of turns:', turns, '\n')
+    print('\nCongratulations! Well done!')
+    print('\nNumber of turns:', turns, '\n')
