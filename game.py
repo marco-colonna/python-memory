3dbe0e69f95c3e83d87fe6d7cbd8c0368a842f47
marco-colonna <mcolonna107@gmail.com>
2021-08-18 18:36:07 -0400

    Split memory.py between memory.py (functions) and game.py (program)

---
+++ game.py
@@ -0,0 +1,49 @@
+import memory
+
+memory.intro()
+
+# represents the face of each card
+cards = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8']
+# represents whether to display the face of the card
+show = [False]*16
+# represents the number of matches
+matches = 0
+# represents the number of turns
+turns = 0
+
+while matches != 8:
+    # print cards
+    memory.print_cards(cards, show)
+
+    # get guess
+    guess1 = memory.guess_card(show)
+    show[guess1] = True
+
+    # print cards
+    memory.print_cards(cards, show)
+
+    # get another guess
+    guess2 = memory.guess_card(show)
+    show[guess2] = True
+
+    # print cards
+    memory.print_cards(cards, show)
+
+    # check guesses
+    card1 = cards[guess1]
+    card2 = cards[guess2]
+
+    if card1 == card2:
+        print('\nMatch! :)')
+        matches += 1
+    else:
+        print('\nNo match. :(')
+        show[guess1] = False
+        show[guess2] = False
+
+    turns += 1
+
+    memory.pause()
+else:
+    print('\nCongratulations! Well done!')
+    print('\nNumber of turns:', turns, '\n')
