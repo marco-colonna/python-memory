e196466a546ff04c40a2d74a4c58a96eb216e682
marco-colonna <mcolonna107@gmail.com>
2021-08-18 16:12:08 -0400

    Format and document `guess_card()` in memory.py

--- memory.py
+++ memory.py
@@ -11,30 +11,30 @@ def print_cards(cards, show):
         if (index + 1) % 4 == 0:
             print() # new line
 
+# get a valid card guess from the user
 def guess_card(show):
     guess = 0
-    while guess == 0:
+    while True:
+        # get a guess from the user
         guess = input('\nGuess a card: ')
-        # numeric
+        # validate guess
+        # check: non-numeric
         if guess.isnumeric() == False:
             print('\nInvalid input. Please try again.')
-            guess = 0
             continue
-        
+        # convert string into an integer
         guess = int(guess)
-        # out of bounds
+        # check: out of bounds
         if guess < 1 or guess > 16:
             print('\nInvalid input. Please try again.')
-            guess = 0
             continue
-        
-        # already matched/chosen
-        if show[guess - 1]:
-            print('\nInvalid input. Please try again.')
-            guess = 0
-            continue
-    else:
+        # decrement by 1 for zero-based indexing purposes
         guess -= 1
+        # check: already chosen or matched, based on the 'show' list
+        if show[guess]:
+            print('\nInvalid input. Please try again.')
+            continue
+        # passed all checks, valid guess
         return guess
 
 # intro
