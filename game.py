63881725b7961ca43b3a8d479f42ad0fa3f4896c
marco-colonna <mcolonna107@gmail.com>
2021-08-18 23:11:54 -0400

    Format and document game.py

--- game.py
+++ game.py
@@ -1,46 +1,57 @@
-import memory
+# game.py - A game of concentration.
 
-memory.intro()
+import concentration
 
 # represents the face of each card
 cards = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', '7', '7', '8', '8']
-# represents whether to display the face of the card
+# represents whether to show the face of each card
 show = [False]*16
-# represents the number of matches
+# represents the number of matches made
 matches = 0
-# represents the number of turns
+# represents the number of turns taken
 turns = 0
 
-while matches != 8:
-    # print cards
-    memory.print_cards(cards, show)
+# print the welcome screen and instructions
+concentration.intro()
 
-    # get guess
-    guess1 = memory.guess_card(show)
+# the game loops until all the matches have been made
+while matches != 8:
+    # print the 'cards' list based on the 'show' list
+    concentration.print_cards(cards, show)
+
+    # get a valid card guess from the user
+    guess1 = concentration.guess_card(show)
+    # update the 'show' list to show the face of the card when printed
     show[guess1] = True
 
-    # print cards
-    memory.print_cards(cards, show)
+    concentration.print_cards(cards, show)
 
-    # get another guess
-    guess2 = memory.guess_card(show)
+    guess2 = concentration.guess_card(show)
     show[guess2] = True
 
-    # print cards
-    memory.print_cards(cards, show)
+    concentration.print_cards(cards, show)
 
-    # check guesses
+    # check if the guessed cards are a matching pair
     if cards[guess1] == cards[guess2]:
         print('\nMatch! :)')
+        # increment the number of matches made
         matches += 1
     else:
         print('\nNo match. :(')
+        # update the 'show' list to hide the face of the card when printed
         show[guess1] = False
         show[guess2] = False
 
+    # increment the number of turns taken
     turns += 1
 
-    memory.pause()
-else:
+    # check if all the matches have been made
+    # this is to avoid pausing after the game is won
+    if matches != 8:
+        # pause before starting the next turn and allow the user to quit or continue playing
+        concentration.pause()
+
+else: # all the matches have been made
+    # print the win screen and the number of turns
     print('\nCongratulations! Well done!')
     print('\nNumber of turns:', turns, '\n')
